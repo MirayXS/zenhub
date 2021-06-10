72759878fb0f65bdfe2c887d14c3ddbab6794aed
laderite <78981916+laderite@users.noreply.github.com>
2021-06-09 23:49:28 -0700

    Update modlist.lua

--- modlist.lua
+++ modlist.lua
@@ -1,14 +1,1 @@
-ModIDS = {
-    [2624975236] = true, -- ZenHubOwner
-    [2487237545] = true, -- 1NeverAsked
-    [2284968703] = true, -- r_xzse
-    [158805546] = true, -- vai_ds
-    [148066781] = true, -- SoggyBlazeStorm
-    [2627813751] = true, -- ZionHoldingUrLims
-    [2634824728] = true, -- msmhashtag1337
-    [780391516] = true, -- 01XIN10
-    [283223102] = true, -- WizzyTea
-    [2600403084] = true, -- SNIPEDBYRICHMEN
-}
-
-return ModIDS
+if ur reading this your gay
