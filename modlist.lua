4be7c48a9b3a928f726d8519b013ed1550dbfe6e
laderite <78981916+laderite@users.noreply.github.com>
2021-06-09 23:39:25 -0700

    Update modlist.lua

--- modlist.lua
+++ modlist.lua
@@ -1,6 +1,13 @@
 ModIDS = {
-    [2602859692] = true, -- ZenHubOwner
-
+    [2624975236] = true, -- ZenHubOwner
+    [2487237545] = true, -- 1NeverAsked
+    [2284968703] = true, -- r_xzse
+    [158805546] = true, -- vai_ds
+    [148066781] = true, -- SoggyBlazeStorm
+    [2627813751] = true, -- ZionHoldingUrLims
+    [2634824728] = true, -- msmhashtag1337
+    [780391516] = true, -- 01XIN10
+    [283223102] = true, -- WizzyTea
 }
 
 return ModIDS
